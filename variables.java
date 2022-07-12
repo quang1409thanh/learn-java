e788d8a99a40945678e5c48f7208e687ce650fb9
quang1409thanh <97092690+quang1409thanh@users.noreply.github.com>
2022-07-12 16:57:04 +0700

    commit lec 2 variable

---
+++ variables.java
@@ -0,0 +1,11 @@
+public class variables{
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        long x=2112324142432444143L;
+        float y=12.644f;
+        boolean z=true;
+        char symnol='!';
+        String name="Jone";
+        System.out.println("hello "+name+!z);
+    }
+}
